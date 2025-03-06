0a2a0f8ab47bd2b6f1e18860114dd0abb97ae8e5
Mixail Zateev <fanerka1992@yandex.ru>
2025-03-06 22:38:27 +0400

    first commit

---
+++ src/main/java/edme/sales_point/SalesPointApplication.java
@@ -0,0 +1,13 @@
+package edme.sales_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edme/sales_point/SalesPointApplicationTests.java
@@ -0,0 +1,13 @@
+package edme.sales_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
