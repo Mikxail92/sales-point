c7fd07220454c214567c74f835a139dd68a00539
Mixail Zateev <fanerka1992@yandex.ru>
2025-03-25 14:42:51 +0400

    перед пушем

--- src/main/java/edme/sales_point/config/SecurityConfig.java
+++ src/main/java/edme/sales_point/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package edme.sales_point.config;
 
 import edme.sales_point.service.UserAccessService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
